9d915bce52636c970a2702e5c6c803421c9a7e28
Jalberth Mosquera <jmosquera2305@gmail.com>
2026-08-27 13:26:50 +0200

    fix(backend): 🗄️ configure whitenoise, and root of admin
    Add whitenoise library
    
    Configure white noise
    
    fix root admin

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -25,7 +25,7 @@ from .health import api_root, health
 urlpatterns = [
     path('api/', api_root, name='api-root'),
     path('api/health/', health, name='health'),
-    path('admin/', admin.site.urls),
+    path('', admin.site.urls),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
