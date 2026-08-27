9f65e4385bf35b50dcde4c4da3a10c45dd2d1428
Jalberth Mosquera <jmosquera2305@gmail.com>
2026-08-27 13:26:50 +0200

    fix(backend): 🗄️ configure whitenoise, and root of admin
    Add whitenoise library
    
    Configure white noise
    
    fix root admin

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -74,6 +74,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -249,5 +250,13 @@ LOGGING = {
     'root': {
         'handlers': ['console'],
         'level': LOG_LEVEL,
+    },
+}
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
 }
